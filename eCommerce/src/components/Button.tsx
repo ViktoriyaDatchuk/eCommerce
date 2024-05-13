dc9b43874a00d5d57563175d7ae0f602ebea6ab7
Jezhora <jezhora.photo@gmail.com>
2024-05-13 21:36:24 +0300

    refactor: edit main page and Button component

--- eCommerce/src/components/Button.tsx
+++ eCommerce/src/components/Button.tsx
@@ -1,12 +1,10 @@
-import PropTypes from 'prop-types';
-
 interface ButtonProps {
   text: string;
   isPrimary: boolean;
 }
 
 export default function Button({ text, isPrimary }: ButtonProps) {
-  const basicStyle = 'px-2.5 py-1  font-sans font-bold uppercase rounded-md hover:bg-pink-600 ';
+  const basicStyle = 'px-2.5 py-1 font-bold uppercase rounded-md hover:bg-pink-600 ';
   const buttonStyle = isPrimary ? `${basicStyle}  bg-teal-400` : `${basicStyle}  bg-orange-400`;
 
   return (
@@ -15,8 +13,3 @@ export default function Button({ text, isPrimary }: ButtonProps) {
     </button>
   );
 }
-
-Button.propTypes = {
-  text: PropTypes.string.isRequired,
-  isPrimary: PropTypes.bool.isRequired,
-};
