ba35bf6c0543be5baa981484a3782932a0941087
Jezhora <jezhora.photo@gmail.com>
2024-05-13 22:26:47 +0300

    feat: add Logo component

--- eCommerce/src/components/Header.tsx
+++ eCommerce/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import Button from './Button';
+import Logo from './Logo';
 
 export default function Header() {
   return (
     <header className="flex justify-between">
+      <Logo />
       <Button text="find a movie" isPrimary={false} />
       <div className="flex gap-5">
         <Button text="sign in" isPrimary />
---
+++ eCommerce/src/components/Logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <button
+      type="button"
+      className="px-4 py-0.5 border-solid border-2 rounded-md border-orange-400 text-white hover:pointer"
+    >
+      Kino<span className="font-bold text-orange-400">GO-VNO</span>
+    </button>
+  );
+}
