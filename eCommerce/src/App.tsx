a1d63e148103d1a590bcafb0b6c7273fb606262c
Jezhora <jezhora.photo@gmail.com>
2024-05-13 14:18:35 +0300

    feat: add main page

--- eCommerce/src/App.tsx
+++ eCommerce/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Main from './pages/main';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />;
+    </>
+  );
 }
 
 export default App;
