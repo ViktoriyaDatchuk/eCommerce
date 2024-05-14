e4756e37420ac9fb200bbbd591b3f0689e2a6a51
Jezhora <jezhora.photo@gmail.com>
2024-05-14 15:49:12 +0300

    feat: implement 404 page

--- eCommerce/src/App.tsx
+++ eCommerce/src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header/Header';
 import Collection from './pages/Collection';
+import Page404 from './pages/Page404';
 
 function LayoutWithHeader() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route element={<LayoutWithHeader />}>
           <Route path="/" element={<Main />} />
           <Route path="/movie-collection" element={<Collection />} />
+          <Route path="*" element={<Page404 />} />
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
---
+++ eCommerce/src/pages/Page404.tsx
@@ -0,0 +1,9 @@
+export default function Page404() {
+  return (
+    <div className="flex justify-center items-center h-full bg-[url('/bg/bg-404.jpg')] bg-center bg-cover">
+      <h1 className="p-8 text-5xl font-bold text-pink-600 border-2 rounded-lg border-pink-600 bg-gray-900">
+        Ops... This page not found
+      </h1>
+    </div>
+  );
+}
