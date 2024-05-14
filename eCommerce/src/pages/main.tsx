dadb641a85fda5d5aa67f14b5be086ac63fdd2f1
YAUHENI JEZHORA <117091303+Jezhora@users.noreply.github.com>
2024-05-14 16:01:39 +0300

    feat: implement 404 page (#62)

--- eCommerce/src/pages/main.tsx
+++ eCommerce/src/pages/main.tsx
@@ -2,9 +2,9 @@ export default function Main() {
   return (
     <div className="flex flex-col justify-center  items-center flex-1 bg-[url('/bg/bg-home.png')] bg-center bg-cover">
       <div className="max-w-3xl w-full py-10 mt-40 font-sans text-white  bg-gray-900 rounded-md">
-        <p className="text-7xl mb-4">
+        <h1 className="text-7xl mb-4">
           Kino<span className="font-bold text-orange-400">GO-VNO</span>
-        </p>
+        </h1>
         <div className="text-2xl font-bold text-teal-400">
           <p>Blast from the Past:</p>
           <p>Blu-ray Movies - The Ancient Relic of Cinematic Enjoyment</p>
