44f29164f374c03f360f6cd1202292de7fedf008
Viktoryia Datchuk <97282032+ViktoriyaDatchuk@users.noreply.github.com>
2024-05-16 23:50:21 +0300

    RSS-ECOMM-2_08/AddButtonToNavigateRegistrationPage (#66)
    * feat: add submit handler in signIn form
    
    * feat: add navigate to the registration page

--- eCommerce/src/components/SignInForm.tsx
+++ eCommerce/src/components/SignInForm.tsx
@@ -1,5 +1,6 @@
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 
 interface FormSignIn {
@@ -10,10 +11,17 @@ interface FormSignIn {
 export default function SignInForm() {
   const {
     register,
-    // handleSubmit,
+    handleSubmit,
     formState: { errors },
   } = useForm<FormSignIn>({ mode: 'all' });
+
+  const onSubmit: SubmitHandler<FormSignIn> = (data) => {
+    console.log(data);
+  };
+
   const [visible, setVisible] = useState(false);
+
+  const navigate = useNavigate();
 
   const labelStyleGeneral = 'text-teal-400 text-xl text-left flex flex-col mt-3.5 mx-20';
   const labelStyle = 'text-teal-400 text-xl text-left flex flex-col mt-7 mx-20 relative';
@@ -24,7 +32,7 @@ export default function SignInForm() {
   const closeEye = `bg-[url('/bg/bg-close.svg')] ${eye}`;
 
   return (
-    <form className="flex flex-col w-full max-w-lg mx-auto">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full max-w-lg mx-auto">
       <h1 className="text-white sm:text-4xl sm:mb-3 font-semibold text-3xl mb-0.5">
         Kino<span className="font-black text-orange-400">GO-VNO</span>
       </h1>
@@ -79,14 +87,14 @@ export default function SignInForm() {
       </label>
       {errors.password && <div className={errorStyle}>{errors.password.message}</div>}
       <div className="flex gap-3.5 mt-16 mx-auto">
-        <Button text="Back" isPrimary />
-        <Button text="Sign In" isPrimary />
+        <Button text="Back" isPrimary onClick={() => {}} />
+        <Button text="Sign In" isPrimary onClick={handleSubmit(onSubmit)} />
       </div>
       <p className="text-orange-400 mt-8">
         If you don&apos;t have an account, you can{' '}
-        <a href="https://www.google.com/" className="underline">
+        <button type="button" onClick={() => navigate('/sign-up')} className="underline">
           sign up here
-        </a>
+        </button>
       </p>
     </form>
   );
