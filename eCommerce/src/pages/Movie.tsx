d02b9dcbc3ebe74b52cd46ebd465a300ee376b3c
Viktoriya Datchuk <grelada@tut.by>
2024-05-31 16:38:16 +0300

    feat: add film description to movie page

--- eCommerce/src/pages/Movie.tsx
+++ eCommerce/src/pages/Movie.tsx
@@ -1,22 +1,33 @@
+import Button from '../components/Button';
 import Page from '../components/Page';
 import { AttributeMovie, movie } from '../data/movies';
 
 export default function Movie() {
+  const containerStyle = 'max-w-[1309px] flex flex-col items-center sm:flex-row sm:items-stretch gap-8 my-4 mx-4';
+  const imageStyle = 'max-w-[300px] drop-shadow-[0_4px_30px_rgba(24,201,176,1)] rounded-xl';
+  const descriptionContainerStyle = 'flex flex-col justify-between my-1 mx-11';
+  const descriptionStyle = 'flex flex-col items-start gap-5 text-white font-medium ml-7 mt-3';
+  const parStyle = 'text-left mb-4';
+  const secParStyle = 'mt-5 text-start';
   const titleClass = 'text-[#FEA732]';
+  const priceContainerStyle = 'flex flex-col sm:flex-row gap-8 lg:gap-20 mt-5 font-extrabold';
+  const priceBlockStyle = 'flex self-center gap-4 text-base lg:text-2xl';
+  const priceStyle = `${titleClass} font-medium`;
+  const priceCrossStyle = `${priceStyle} line-through`;
+
+  const getPrice = (price: number, digit: number, code: string): string => {
+    return `${price / 10 ** digit} ${code}`;
+  };
 
   return (
-    <Page className="flex justify-center items-center h-full">
-      <div className="max-w-[1309px] flex gap-8">
+    <Page className="flex justify-center items-center h-max sm:h-full">
+      <div className={containerStyle}>
         <div>
-          <img
-            src={movie.masterData.current.masterVariant.images[0].url}
-            alt="movie"
-            className="max-w-[300px] drop-shadow-[0_4px_30px_rgba(24,201,176,1)] rounded-xl"
-          />
+          <img src={movie.masterData.current.masterVariant.images[0].url} alt="movie" className={imageStyle} />
         </div>
-        <div className="my-5 mx-9">
-          <div className="flex flex-col items-start gap-8 text-white font-medium">
-            <p className="text-left">
+        <div className={descriptionContainerStyle}>
+          <div className={descriptionStyle}>
+            <p className={parStyle}>
               {
                 movie.masterData.current.masterVariant.attributes.find(
                   (value: AttributeMovie) => value.name === 'description'
@@ -42,7 +53,7 @@ export default function Movie() {
                 )?.value
               }
             </p>
-            <p className="mt-3">
+            <p className={secParStyle}>
               <span className={titleClass}>Actors: </span>
               {
                 movie.masterData.current.masterVariant.attributes.find(
@@ -51,7 +62,38 @@ export default function Movie() {
               }
             </p>
           </div>
-          <div />
+          <div className={priceContainerStyle}>
+            <div className={priceBlockStyle}>
+              <p
+                className={
+                  movie.masterData.current.masterVariant.prices[0].discounted.value.centAmount
+                    ? priceCrossStyle
+                    : priceStyle
+                }
+              >
+                {getPrice(
+                  movie.masterData.current.masterVariant.prices[0].value.centAmount,
+                  movie.masterData.current.masterVariant.prices[0].value.fractionDigits,
+                  movie.masterData.current.masterVariant.prices[0].value.currencyCode
+                )}
+              </p>
+              <p className="text-[#FC2A66]">
+                {getPrice(
+                  movie.masterData.current.masterVariant.prices[0].discounted.value.centAmount,
+                  movie.masterData.current.masterVariant.prices[0].discounted.value.fractionDigits,
+                  movie.masterData.current.masterVariant.prices[0].discounted.value.currencyCode
+                )}
+              </p>
+            </div>
+            <Button
+              text="Add disk"
+              isPrimary={false}
+              onClick={() => {
+                console.log('Buy');
+              }}
+              addClass="lg:px-12"
+            />
+          </div>
         </div>
       </div>
     </Page>
