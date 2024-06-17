de67c395c257d9012b3fb392844193cd4884b208
proboynick <kuzmi495@mail.ru>
2024-06-17 21:26:34 +0300

    feat: add cart item card

---
+++ eCommerce/src/components/CartItem.tsx
@@ -0,0 +1,45 @@
+import Button from './Button';
+
+interface CartItemProps {
+  imgLink: string;
+  name: string;
+}
+const priceStyle = 'text-decoration-line: line-through text-orange-400';
+const controlsStyle = 'text-orange-400 text-2xl';
+const discountPriceStyle = 'text-red-500';
+const labelStyle = 'text-teal-400 text-2xl';
+export default function CartItem({ imgLink, name }: CartItemProps) {
+  return (
+    <div className="flex flex-row justify-between">
+      <img
+        src={imgLink}
+        alt=""
+        style={{ height: '240px', width: '160px', aspectRatio: '3/4', borderRadius: '10px', objectFit: 'cover' }}
+      />
+      <div className={labelStyle} style={{ width: '240px', paddingTop: '60px' }}>
+        <p className=" uppercase">{name}</p>
+        <div style={{ margin: '20px 0' }}>
+          <p className={priceStyle}>€10.99</p>
+          <p className={discountPriceStyle}>€9.34</p>
+        </div>
+      </div>
+      <div style={{ paddingTop: '60px' }}>
+        <p className={labelStyle}>quantity</p>
+        <div className={controlsStyle} style={{ margin: '20px 0' }}>
+          <button>-</button>
+          <span className="mx-4">1</span>
+          <button>+</button>
+        </div>
+      </div>
+      <div style={{ paddingTop: '60px' }}>
+        <p className={labelStyle}>total</p>
+        <p className={controlsStyle} style={{ margin: '20px 0' }}>
+          €15.00
+        </p>
+      </div>
+      <div style={{ paddingTop: '60px' }}>
+        <Button text={'Remove disc'} isPrimary={false} addClass="max-h-10 bg-pink-600"></Button>
+      </div>
+    </div>
+  );
+}
--- eCommerce/src/pages/Cart/Cart.tsx
+++ eCommerce/src/pages/Cart/Cart.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import { LineItem } from '@commercetools/platform-sdk';
+import CartItem from '../../components/CartItem';
 import Page from '../../components/Page';
+import apiRoot from '../../sdk/apiRoot';
+import useCurrentUser from '../../user/getCurrentUser';
 
 export default function Cart() {
+  const [cartItems, setCartItems] = useState<LineItem[]>();
+  const getCart = async () => {
+    const user = useCurrentUser();
+    if (!user) {
+      return;
+    }
+    const responseCustomerCart = await apiRoot.carts().withCustomerId({ customerId: user.id }).get().execute();
+    if (!responseCustomerCart) {
+      return;
+    }
+    setCartItems(responseCustomerCart.body.lineItems);
+    console.log(cartItems);
+  };
+  getCart();
   return (
     <Page>
-      <h1 className="text-white">CART</h1>
+      <div className="flex flex-col mt-20 gap-10">
+        {/* <h1 className="text-white">CART</h1> */}
+        {cartItems?.map((item) => <CartItem imgLink={item.variant.images![0].url} name={item.name['en-US']} />)}
+      </div>
     </Page>
   );
 }
