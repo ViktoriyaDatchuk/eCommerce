3a13625074ad7a35ad6f85f8f6fe4c22951f05e3
Viktoryia Datchuk <97282032+ViktoriyaDatchuk@users.noreply.github.com>
2024-06-17 23:24:11 +0300

    Feat/create about us page (#122)
    * feat: create about us page
    
    * feat: create team member component, add members of team to about us page
    
    * feat: add rsschool site link, add team members info

---
+++ eCommerce/src/components/TeamMember.tsx
@@ -0,0 +1,23 @@
+import { TeamItem } from '../data/team';
+
+export default function TeamMember({ image, name, role, github, description }: TeamItem) {
+  const teamStyle = 'flex flex-col items-center md:max-w-[30%]';
+  const teamItemStyle = `${teamStyle} mt-16`;
+  const teamLeadStyle = `${teamStyle} order-first md:order-none`;
+  const imageStyle = 'w-40 h-40 object-cover rounded-[50%]';
+  const nameStyle = 'font-bold text-lg mt-2 text-orange-400';
+  const roleStyle = 'font-bold text-[#CF8C54]';
+  const githubLinkStyle = 'text-[#A47E5F] font-bold hover:text-pink-600 transition ease-in delay-50';
+
+  return (
+    <div className={role === 'Team Lead' ? teamLeadStyle : teamItemStyle}>
+      <img src={image} alt={image} className={imageStyle} />
+      <h4 className={nameStyle}>{name}</h4>
+      <p className={roleStyle}>{role}</p>
+      <a href={github} className={githubLinkStyle}>
+        GitHub
+      </a>
+      <p className="mt-2 text-sm">{description}</p>
+    </div>
+  );
+}
---
+++ eCommerce/src/pages/AboutUS.tsx
@@ -0,0 +1,35 @@
+import Page from '../components/Page';
+import TeamMember from '../components/TeamMember';
+import { team } from '../data/team';
+
+export default function AboutUs() {
+  const pageStyle = 'flex flex-col w-full items-center text-teal-400 gap-1';
+  const teamContainerStyle = 'flex justify-between my-6 mx-6 flex-col md:flex-row';
+  const svgStyle =
+    'hover:invert-[.27] hover:sepia-[.84] hover:saturate-[34.75] hover:hue-rotate-[328deg] hover:brightness-[1.02] hover:contrast-[.98]';
+
+  return (
+    <Page>
+      <div className={pageStyle}>
+        <h2 className="text-3xl font-bold">Smorgon Mafia</h2>
+        <p className="text-xl">Smorgon team - со Сморгони двое, из Минска один.</p>
+        <p>Our close-knit team spent many sleepless nights together creating this unforgettable online store.</p>
+        <div className={teamContainerStyle}>
+          {team.map((item) => (
+            <TeamMember
+              image={item.image}
+              name={item.name}
+              role={item.role}
+              github={item.github}
+              description={item.description}
+              key={item.name}
+            />
+          ))}
+        </div>
+        <a className="self-end mr-2 mb-2" href="https://rs.school/" target="_blank">
+          <img className={svgStyle} src="/github.svg" alt="github" />
+        </a>
+      </div>
+    </Page>
+  );
+}
