ec62a575033aae5a573db761f8aed16137aa8516
proboynick <kuzmi495@mail.ru>
2024-06-17 21:46:22 +0300

    feat: update props in cart item card

--- eCommerce/src/pages/Cart/Cart.tsx
+++ eCommerce/src/pages/Cart/Cart.tsx
@@ -17,14 +17,22 @@ export default function Cart() {
       return;
     }
     setCartItems(responseCustomerCart.body.lineItems);
-    console.log(cartItems);
   };
   getCart();
   return (
     <Page>
       <div className="flex flex-col mt-20 gap-10">
-        {/* <h1 className="text-white">CART</h1> */}
-        {cartItems?.map((item) => <CartItem imgLink={item.variant.images![0].url} name={item.name['en-US']} />)}
+        <h1 className="text-white">CART</h1>
+        {cartItems?.map((item) => (
+          <CartItem
+            key={item.id}
+            imgLink={item.variant.images![0].url}
+            name={item.name['en-US']}
+            priceValue={`${item.price.value.centAmount / 100}`}
+            discountedPrice={`${item.price.discounted?.value.centAmount ? item.price.discounted.value.centAmount / 100 : 'No discount'}`}
+            totalPrice={`${item.totalPrice.centAmount / 100}`}
+          />
+        ))}
       </div>
     </Page>
   );
