6cb0f4d6ff275a2ec1a2478e7fbea810541cfff1
Viktoriya Datchuk <grelada@tut.by>
2024-06-17 23:07:22 +0300

    feat: add rsschool site link, add team members info

--- eCommerce/src/data/team.ts
+++ eCommerce/src/data/team.ts
@@ -3,7 +3,6 @@ export interface TeamItem {
   name: string;
   role: string;
   github: string;
-  bio: string;
   description: string;
 }
 
@@ -13,24 +12,23 @@ export const team: TeamItem[] = [
     name: 'Anton Kuziukou',
     role: "Site's name creator",
     github: 'https://github.com/proboynick',
-    bio: 'Bla-bla-bla',
-    description: 'Genius, playboy, philanthropist, still not a billionaire. Form and Slow Loading tamer',
+    description:
+      '29 yo, currently PCB designer and scientific researcher. Genius, playboy, philanthropist, still not a billionaire. Form and Slow Loading tamer',
   },
   {
     image: '/team/Vika.jpg',
     name: 'Viktoryia Datchuk',
     role: 'Team Lead',
     github: 'https://github.com/viktoriyadatchuk',
-    bio: '35 years old. In past customs officer and licensing engineer. Currently a florist, barista and junior front-end developer.',
     description:
-      'Big Boss. Sweet on the outside, devil on the inside. Team organizer and encourager. API-creator, holder of the beast called Token, commander of  sliders.',
+      '35 yo. In past customs officer and licensing engineer, currently a florist, barista and junior front-end developer. Big Boss. Sweet on the outside, devil on the inside. Team organizer and encourager. API-creator, holder of the beast called Token, commander of  sliders.',
   },
   {
-    image: '/team/Zhenia.jpg',
+    image: '/team/Zhenia.png',
     name: 'Yauheni Jezhora',
     role: 'Commercetools conflict resolution specialist',
     github: 'https://github.com/jezhora',
-    bio: 'Bla-bla-bla',
-    description: 'Bla-bla-bla',
+    description:
+      "Found out if the frontend exists after 39. Used to be a photographer, nowadays works as lead specialist in foreign economic activity. His future will bind with frontend development. Made App design despite of he've ever never touch to figma. If you are reading this message he has not defeated the 409 error. In youth drank with team lead.",
   },
 ];
--- eCommerce/src/pages/AboutUS.tsx
+++ eCommerce/src/pages/AboutUS.tsx
@@ -3,25 +3,32 @@ import TeamMember from '../components/TeamMember';
 import { team } from '../data/team';
 
 export default function AboutUs() {
+  const pageStyle = 'flex flex-col w-full items-center text-teal-400 gap-1';
+  const teamContainerStyle = 'flex justify-between my-6 mx-6 flex-col md:flex-row';
+  const svgStyle =
+    'hover:invert-[.27] hover:sepia-[.84] hover:saturate-[34.75] hover:hue-rotate-[328deg] hover:brightness-[1.02] hover:contrast-[.98]';
+
   return (
     <Page>
-      <div className="flex flex-col w-full items-center text-teal-400 gap-1">
+      <div className={pageStyle}>
         <h2 className="text-3xl font-bold">Smorgon Mafia</h2>
         <p className="text-xl">Smorgon team - со Сморгони двое, из Минска один.</p>
         <p>Our close-knit team spent many sleepless nights together creating this unforgettable online store.</p>
-        <div className="flex justify-between my-6 mx-6 flex-col md:flex-row">
+        <div className={teamContainerStyle}>
           {team.map((item) => (
             <TeamMember
               image={item.image}
               name={item.name}
               role={item.role}
               github={item.github}
-              bio={item.bio}
               description={item.description}
               key={item.name}
             />
           ))}
         </div>
+        <a className="self-end mr-2 mb-2" href="https://rs.school/" target="_blank">
+          <img className={svgStyle} src="/github.svg" alt="github" />
+        </a>
       </div>
     </Page>
   );
