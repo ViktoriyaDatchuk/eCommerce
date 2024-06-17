e1194d4d139eaaad81e5395a0b5b998976f0852b
Antony <kuzmi495@mail.ru>
2024-06-17 23:26:55 +0300

    feat: add message at the empty cart

--- eCommerce/src/pages/Cart/Cart.tsx
+++ eCommerce/src/pages/Cart/Cart.tsx
@@ -4,6 +4,7 @@ import CartItem from '../../components/CartItem';
 import Page from '../../components/Page';
 import apiRoot from '../../sdk/apiRoot';
 import useCurrentUser from '../../user/getCurrentUser';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
   const [cartItems, setCartItems] = useState<LineItem[]>();
@@ -22,17 +23,30 @@ export default function Cart() {
   return (
     <Page>
       <div className="flex flex-col mt-20 gap-10">
-        <h1 className="text-white">CART</h1>
-        {cartItems?.map((item) => (
-          <CartItem
-            key={item.id}
-            imgLink={item.variant.images![0].url}
-            name={item.name['en-US']}
-            priceValue={`${item.price.value.centAmount / 100}`}
-            discountedPrice={`${item.price.discounted?.value.centAmount ? item.price.discounted.value.centAmount / 100 : 'No discount'}`}
-            totalPrice={`${item.totalPrice.centAmount / 100}`}
-          />
-        ))}
+        <h1 className="text-white text-2xl">CART</h1>
+        {cartItems && cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <CartItem
+              key={item.id}
+              imgLink={item.variant.images![0].url}
+              name={item.name['en-US']}
+              priceValue={`${item.price.value.centAmount / 100}`}
+              discountedPrice={`${item.price.discounted?.value.centAmount ? item.price.discounted.value.centAmount / 100 : 'No discount'}`}
+              totalPrice={`${item.totalPrice.centAmount / 100}`}
+            />
+          ))
+        ) : (
+          <div className="text-white text-2xl">
+            <p>Your cart is empty</p>
+            <p>
+              Go to{' '}
+              <Link to={'/movie-collection'} className="underline text-orange-500">
+                Catalog page
+              </Link>{' '}
+              to add any product
+            </p>
+          </div>
+        )}
       </div>
     </Page>
   );
